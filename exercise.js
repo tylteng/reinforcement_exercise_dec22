c5a4ee42c429919dad66adf7e5416c619a325c91
tylteng <teng.ty4@gmail.com>
2017-12-24 22:30:19 -0500

    completed exercise

---
+++ exercise.js
@@ -0,0 +1,6 @@
+var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
+
+for(var i = 0; i < items.length; i++){
+  console.log("Todo:");
+  console.log((i+1) + ': ' + items[i]);
+}
